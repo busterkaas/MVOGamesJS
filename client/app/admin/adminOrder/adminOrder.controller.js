8e8e0596b07e121fe4b46c0aa1774dcc702f8184
Kennie <the_reborn92@hotmail.com>
2016-05-03 12:09:46 +0200

    orderservice and other order stuff

--- client/app/admin/adminOrder/adminOrder.controller.js
+++ client/app/admin/adminOrder/adminOrder.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('mvogamesJsApp')
-  .controller('AdminOrderCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('AdminOrderCtrl', function ($scope, OrderService, socket) {
+
+    OrderService.query(function(orders){
+      $scope.Orders = orders;
+      socket.syncUpdates('order', $scope.Orders);
+    });
+
   });

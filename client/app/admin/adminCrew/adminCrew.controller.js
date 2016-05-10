374d319891247bb20fc051ea45ff2821f0eece1b
Tasina <tasin-a@hotmail.com>
2016-05-10 10:13:12 +0200

    crew

--- client/app/admin/adminCrew/adminCrew.controller.js
+++ client/app/admin/adminCrew/adminCrew.controller.js
@@ -1,8 +1,7 @@
 'use strict';
 
 angular.module('mvogamesJsApp')
-  .controller('AdminCrewCtrl', function ($scope, CrewService, socket, $mdDialog) {
-
+  .controller('AdminCrewCtrl', function ($scope, CrewService, socket, $mdDialog, $mdToast) {
 
     CrewService.query(function(crews){
       $scope.Crews = crews;
@@ -11,9 +10,11 @@ angular.module('mvogamesJsApp')
 
     $scope.editCrew = function(crew){
       $scope.editingCrew = crew;
+      $scope.copy = angular.copy($scope.editingCrew);
     };
 
     $scope.undoEditCrew = function(){
+      $scope.editingCrew = $scope.copy;
       $scope.editingCrew = undefined;
     };
 
@@ -36,6 +37,35 @@ angular.module('mvogamesJsApp')
       }, function() {
       //do nothing
         });
+    };
+
+    $scope.deleteUserFromCrew = function(crew, ev){
+      var confirm = $mdDialog.confirm()
+      .title('Delete User from Crew')
+      .textContent('Are you sure you want to delete this user')
+      .ariaLabel('Delete')
+      .targetEvent(ev)
+      .openFrom('#left')
+      .ok('YES, I am sure!')
+      .cancel('No');
+      $mdDialog.show(confirm).then(function(){
+        _.remove($scope.crew.users, function(u){
+          return u._id === $scope.editingCrew._id;
+        });
+
+        CrewService.update({
+          id: $scope.crew._id
+        }, $scope.crew, function(crew){
+          $scope.crew = crew;
+          var toast = $mdToast.simple()
+          .textContent('User deleted')
+          .action('Ok')
+          .highlightAction(false)
+          .position('top');
+          $mdToast.show(toast);
+          $scope.editingCrew = undefined;
+        });
+      });
     };
 
     $scope.editGameSuggestion = function(gameSuggestions) {
